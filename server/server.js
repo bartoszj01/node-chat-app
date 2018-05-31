8b6053f31e58f67ad4b963c827ab8cce8dc9534c
Bartosz Jarmolinski <b.jarmolinski@sonnen.de>
2018-05-31 16:03:50 +0200

    add connect and disconnect event handles

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 var app = express();
 const port = process.env.PORT || 3000;
-
-// app.get('', (req, res) => {
-//     res.sendfile('public/index.html', publicPath);
-// });
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', ()  => {
+        console.log('client disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
 
