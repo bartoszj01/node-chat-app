147f8310dffed030d7723c632c7d46ec671ed475
Bartosz Jarmolinski <b.jarmolinski@sonnen.de>
2018-06-22 13:12:05 +0200

    add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit('newEmail', {
+    //     from: 'mike@example.com',
+    //     text: 'example text',
+    //     createAt: 123
+    // });
+
+    socket.emit('newMessage', {
+        from: 'ba@gmail.com',
+        text: 'message 1',
+        createdAt: 234
+    });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', ()  => {
         console.log('client disconnected');
     });
